8217183e117a428f2fc6d50511c332c84996aee4
tararosenthal <65468485+tararosenthal@users.noreply.github.com>
2020-05-19 14:08:36 -0500

    Add files via upload

---
+++ DeletePersonDTO.java
@@ -0,0 +1,11 @@
+package com.codegym.task.task17.task1711;
+
+public class DeletePersonDTO implements PersonDTO{
+    public int id;
+
+    public DeletePersonDTO() {}
+
+    public DeletePersonDTO(int id) {
+        this.id = id;
+    }
+}
