1cadb144830dc2683a51d6410e12a59c19ecb830
tararosenthal <65468485+tararosenthal@users.noreply.github.com>
2020-05-19 14:08:54 -0500

    Add files via upload

---
+++ IndexPersonDTO.java
@@ -0,0 +1,11 @@
+package com.codegym.task.task17.task1711;
+
+public class IndexPersonDTO implements PersonDTO{
+    public int id;
+
+    public IndexPersonDTO() {}
+
+    public IndexPersonDTO(int id) {
+        this.id = id;
+    }
+}
