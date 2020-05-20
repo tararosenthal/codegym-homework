25cf3bc3a575bf43f96cbf6040f64a91ab45421b
tararosenthal <65468485+tararosenthal@users.noreply.github.com>
2020-05-20 06:03:59 -0500

    Add files via upload

--- CommandParser.java
+++ CommandParser.java
@@ -2,42 +2,43 @@ package com.codegym.task.task17.task1711;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/* I realized that the way I did this does not allow me to return multiple objects using the parseCommand method.
-Also my brain is fried, so I'm gonna get back to this tommorow...
-
-Actually, I fixed something stupid, but my brain is still fried, hahaha
-*/
-
 public class CommandParser {
 
-    public static PersonDTO parseCommand(PersonDTO personDTO, String[] input) throws ParseException {
-        if (personDTO == null) { parseNextPerson(input);}
-        return personDTO;
+    static ArrayList<PersonDTO> personDTOs = new ArrayList<>();
+
+    public static ArrayList<PersonDTO> parseCommand(String[] input) throws ParseException {
+        parseNextPerson(input);
+        return personDTOs;
     }
 
     public static void createPerson(String name, String sex, Date birthDate, String[] input) throws ParseException {
         CreatePersonDTO createPersonDTO = new CreatePersonDTO(name, sex, birthDate);
-        parseCommand(createPersonDTO, input);
+        personDTOs.add(createPersonDTO);
+        parseNextPerson(input);
     }
 
     public static void updatePerson(int id, String name, String sex, Date birthDate, String[] input) throws ParseException {
         UpdatePersonDTO updatePersonDTO = new UpdatePersonDTO(id, name, sex, birthDate);
-        parseCommand(updatePersonDTO, input);
+        personDTOs.add(updatePersonDTO);
+        parseNextPerson(input);
     }
 
     public static void deletePerson(int id, String[] input) throws ParseException {
         DeletePersonDTO deletePersonDTO = new DeletePersonDTO(id);
-        parseCommand(deletePersonDTO, input);
+        personDTOs.add(deletePersonDTO);
+        parseNextPerson(input);
     }
 
     public static void indexPerson(int id, String[] input) throws ParseException {
         IndexPersonDTO indexPersonDTO = new IndexPersonDTO(id);
-        parseCommand(indexPersonDTO, input);
+        personDTOs.add(indexPersonDTO);
+        parseNextPerson(input);
     }
 
     public static Date parseBirthDate(String birthDate) throws ParseException {
@@ -45,7 +46,7 @@ public class CommandParser {
     }
 
     public static boolean isBirthDate(String string) {
-        Pattern pattern = Pattern.compile("\\d+\\s\\d+\\s\\d+");
+        Pattern pattern = Pattern.compile("\\d\\d\\s\\d\\d\\s\\d\\d\\d\\d");
         Matcher matcher = pattern.matcher(string);
         return matcher.find();
     }
@@ -76,10 +77,8 @@ public class CommandParser {
             if(isBirthDate(input[3])) {birthDate = parseBirthDate(input[3]);}
             else {throw new ParseException(input[3],0);}
 
-            String[] remaining = new String[input.length - 3];
-            remaining[0] = "-c";
-            if(input.length > 4) {
-            System.arraycopy(input, 4, remaining, 1, input.length - 3);}
+            String[] remaining = remainingArray(input, 3, input[0]);
+
             createPerson(name, sex, birthDate, remaining);
         }
         else if(input[0].contentEquals("-u")) {
@@ -91,23 +90,28 @@ public class CommandParser {
             if(isBirthDate(input[4])) {birthDate = parseBirthDate(input[4]);}
             else {throw new ParseException(input[4],0);}
 
-            String[] remaining = new String[input.length - 4];
-            remaining[0] = "-u";
-            if(input.length > 5) {
-                System.arraycopy(input, 5, remaining, 1, input.length - 4);}
+            String[] remaining = remainingArray(input, 4, input[0]);
+
             updatePerson(id, name, sex, birthDate, remaining);
         }
         else if(input[0].contentEquals("-i") || input[0].contentEquals("-d")) {
             if(isID(input[1])) {id = Integer.parseInt(input[1]);}
             else {throw new ParseException(input[1],0);}
 
-            String[] remaining = new String[input.length - 1];
-            remaining[0] = "-i";
-            if(input.length > 2) {
-                System.arraycopy(input, 2, remaining, 1, input.length - 1);}
+            String[] remaining = remainingArray(input, 1, input[0]);
+
             if(input[0].contentEquals("-i")) { indexPerson(id, remaining);}
             else {deletePerson(id, remaining);}
         }
         else {throw new ParseException(input[0],0);}
+    }
+
+    private static String[] remainingArray(String[] current, int offset, String flag) {
+        String[] remaining = new String[current.length - offset];
+        remaining[0] = flag;
+        if(current.length > ++offset) {
+            System.arraycopy(current, offset, remaining, 1, current.length - offset);}
+
+        return remaining;
     }
 }
